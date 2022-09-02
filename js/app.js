b76dffd38a8d19b3f502d87df65df09cc88fb67b
Hernán Chamorro <hernang.work@gmail.com>
2022-09-02 18:27:11 -0300

    limpiar_html

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ const vaciarCarritoBtn = document.querySelector('#vaciar-carrito')
 let listaCurso = document.querySelector('#lista-cursos')
 let cursos = document.querySelectorAll('.card')
 let articulosCarro = []
-
 cargarEventListeners()
 function cargarEventListeners () {
     // dispara cuando se presiona agregar al carrito
@@ -30,6 +29,7 @@ function leerDatosCurso (element) {
         imagen: element.querySelector('img').src,
         titulo: element.querySelector('h4').textContent,
         precio: element.querySelector('.precio span').textContent,
+        cantidad: 1,
         id: element.querySelector('a').getAttribute('data-id')
     }
 
@@ -37,4 +37,31 @@ function leerDatosCurso (element) {
 }
 
 
+// Mostrar el carrito de compras con los elementos añadidos
+function mostrarCarrito() {
 
+    // limpiar el html
+    
+    articulosCarro.forEach(curso => {
+        const row = document.createElement('tr')
+        row.innerHTML = `
+            <td>
+                <img src="${curso.imagen}" width="100">
+            </td>
+            <td>${curso.titulo}</td>
+            <td>${curso.precio}</td>
+            <td>${curso.cantidad}</td>
+            `
+        // agrega el html del carrito al tbody
+        contenedorCarrito.appendChild(row)
+    })
+}
+
+// Eliminar los cursos del tbody
+function limpiarHTML() {
+    // forma lenta
+    // contenedorCarrito.innerHTML = ''
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
+    }
+}
